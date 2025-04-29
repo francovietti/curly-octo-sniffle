90b8a58814aa89745612192e8b7e53d37ac5a357
francovietti <francovietti08@gmail.com>
2025-04-29 08:45:20 -0300

    Delete app directory

--- app/logout.js
+++
@@ -1,9 +0,0 @@
-import { signOut } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js"
-import { auth } from "./firebase.js"
-
-const logout = document.querySelector("#logout")
-
-logout.addEventListener("click" , async () => {
-    await signOut(auth)
-    console.log("user sign out")
-})
